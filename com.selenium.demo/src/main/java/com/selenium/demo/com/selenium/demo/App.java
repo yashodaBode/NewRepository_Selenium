46a0ce7049dda495cfa98b60b20cc176dd2fe8d7
Yashoda Bode <yashoda.bode@sap.com>
2018-05-21 12:35:18 +0530

    changes done
    Signed-off-by: Yashoda Bode <yashoda.bode@sap.com>

---
+++ com.selenium.demo/src/main/java/com/selenium/demo/com/selenium/demo/App.java
@@ -0,0 +1,13 @@
+package com.selenium.demo.com.selenium.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello yashoda!" );
+    }
+}
